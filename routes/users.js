b5d43a333e042851e8d4d5a69ebb904aa1d3b929
KyeongSoo Yoo <KyeongSoo Yoo>
2017-02-04 14:18:59 +0900

    signin

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,15 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  res.render("./users/signin" , { title : "SIGN IN" });
 });
+
+router.get('/signup' , function(req,res,next){
+  res.render('./users/signup' , { title : "SIGN UP" });
+})
+
+router.get('/signin' , function(req,res,next){
+  res.render('./users/signin' , { title : "SIGN IN" });
+})
 
 module.exports = router;
