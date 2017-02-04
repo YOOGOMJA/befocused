5c6578a259243d0e8997ce7ff351925473b6b634
KyeongSoo Yoo <KyeongSoo Yoo>
2017-02-04 21:33:13 +0900

    login made

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,80 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-var users = require('../db/users.js');
+var User = require('../db/users.js');
+
+module.exports = function(){
+    passport.serializeUser(function(user , done){
+        done(null , user.id);
+    });
+    
+	passport.deserializeUser(function(id , done){
+		User.findById(id , function(err , user){
+            done(err , user);
+        })
+	});
+
+    // ===========================
+	// LOCAL
+	// ===========================
+	passport.use("local-signup" , new LocalStrategy({
+		usernameField : "inputEmail",
+		passwordField : "inputPwValid1",
+		passReqToCallback : true
+	} , function(req , email , password , done){
+		
+		process.nextTick(function(){
+
+			// 패스워드 Valid체크 
+			var body = req.body;
+
+			if(body.inputPwValid1.trim() != body.inputPwValid2){
+				return done(null , false, req.flash('signupMessage' , '패스워드가 일치하지 않습니다'));
+			}
+			/*
+            if(!User.secureCheck(body.inputPassword)){
+				return done(null , false, req.flash('signupMessage' , '유효한 패스워드가 아닙니다 규칙을 확인해주세요'));
+			}
+            */
+			
+			User.findOne({ "LOCAL.EMAIL" : email } , function(err , user){
+				if(err){ done(err); }
+				if(user){ return done(null , false , req.flash("signupMessage" , "이 이메일은 이미 사용중입니다")); }
+				else{
+					var newbie = new User();
+					newbie.LOCAL.EMAIL = email;
+					newbie.LOCAL.PASSWORD = newbie.generateHash(password);
+					newbie.INFO.EMAIL = email;
+						
+					newbie.save(function(err){
+						if(err){ throw err; }
+						return done(null , newbie);
+					});
+				}
+			});
+		});
+	}));
+	
+	passport.use("local-signin" , new LocalStrategy({
+		usernameField : "inputEmail",
+		passwordField : "inputPw",
+		passReqToCallback : true
+	} , function(req , email , password , done){
+		
+		User.findOne({ "LOCAL.EMAIL" : email } , function(err , user){
+			if(err){ 
+                console.log("ERR INVOLVED!!");
+                console.log(err);
+                return done(err); 
+            }
+			if(!user){ 
+                return done(null , false , req.flash( "signinMessage" ,  "잘못되거나 없는 이메일입니다")); }
+			if(!user.validPassword(password)){
+				return done(null , false , req.flash( "signinMessage" , "패스워드가 잘못됐습니다" ));
+			}
+            
+			return done(null , user);
+		});
+		
+	}));
+	
+}
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -8,13 +9,27 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/signup' , function(req,res,next){
-  res.render('./users/signup' , { title : "SIGN UP" });
+  
+  res.render('./users/signup' , { title : "SIGN UP"  , message : req.flash('signupMessage') });
 });
 
 
 router.get('/signin' , function(req,res,next){
-  res.render('./users/signin' , { title : "SIGN IN" });
+  console.log(req.flash('sign'))
+  res.render('./users/signin' , { title : "SIGN IN" , message : req.flash('signinMessage') });
 });
 
+router.post("/signin", passport.authenticate("local-signin" , {
+  successRedirect : "/users/profile",
+  failureRedirect : "/users/signin",
+  failureFlash : true
+}));
+
+router.post("/signup",  passport.authenticate("local-signup" , {
+  successRedirect : "/users/profile",
+  failureRedirect : "/users/signup",
+  failureFlash : true
+	}));
+	
 
 module.exports = router;
